b3d81a40a5a959005bfbf2280bc36227f48894fb
estebanmtzrdgz <estebanmartinezrdgz8@gmail.com>
2025-11-05 18:41:41 -0600

    Creacion_ClaseAbstracta_Baraja
    cree la clase abstracta de baraja
    
    Co-Authored-By: SantiagoSpook <171261289+SantiagoSpook@users.noreply.github.com>
    Co-Authored-By: G2Titan <93502813+G2Titan@users.noreply.github.com>

---
+++ JuegoDeCartas/Clases_Abstractas/baraja.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public abstract class Baraja
+{
+    protected List<Carta> cartas;
+
+    public Baraja()
+    {
+        this.cartas = new List<Carta>();
+    }
+
+    public abstract void InicializarBaraja();
+
+    public void Barajear()
+    {
+        Random rng = new Random();
+        this.cartas = this.cartas.OrderBy(c => rng.Next()).ToList();
+    }
+
+    public Carta RepartirCarta()
+    {
+        if (EstaVacia())
+        {
+            return null; 
+        }
+
+        Carta carta = cartas[0];
+        cartas.RemoveAt(0);
+        return carta;
+    }
+
+    public bool EstaVacia()
+    {
+        return cartas.Count == 0;
+    }
+
+    public int CartasRestantes()
+    {
+        return cartas.Count;
+    }
+}
