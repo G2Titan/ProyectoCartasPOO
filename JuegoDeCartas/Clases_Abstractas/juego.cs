9a3c217414ae2d5dbd1e0c2c9f06772313d859cb
estebanmtzrdgz <estebanmartinezrdgz8@gmail.com>
2025-11-07 12:20:46 -0600

    creacion clase abstracta juego
    Se creo la clase abstracta juego la cual se usara para el black jack y uno.

---
+++ JuegoDeCartas/Clases_Abstractas/juego.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+
+public abstract class Juego
+{
+   
+    protected List<Jugador> jugadores;
+    protected Baraja baraja;
+    protected List<Carta> pilaDescarte;
+    protected bool juegoTerminado;
+
+    public Juego()
+    {
+        this.jugadores = new List<Jugador>();
+        this.pilaDescarte = new List<Carta>();
+        this.juegoTerminado = false;
+    }
+
+  
+    public void IniciarSimulacion()
+    {
+        LoggearAccion("--- Iniciando Simulación ---");
+        
+     
+        while (!juegoTerminado)
+        {
+          
+            EjecutarLogicaJuego();
+            
+            
+            juegoTerminado = VerificarCondicionFinJuego();
+        }
+
+        LoggearAccion("--- Simulación Terminada ---");
+        
+        AnunciarGanador();
+    }
+
+   
+    public abstract void ConfigurarJuego(List<IEstrategiaJuego> estrategiasJugadores);
+
+    protected abstract void EjecutarLogicaJuego();
+
+    
+    protected abstract bool VerificarCondicionFinJuego();
+
+   
+    protected abstract void AnunciarGanador();
+
+    
+    public void LoggearAccion(string mensaje)
+    {
+        Console.WriteLine($"[SIM]: {mensaje}");
+    }
+}
