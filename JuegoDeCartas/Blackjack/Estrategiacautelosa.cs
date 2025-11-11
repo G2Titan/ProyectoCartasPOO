183c700d890ad79512143312f05ab1850cc72b14
estebanmtzrdgz <estebanmartinezrdgz8@gmail.com>
2025-11-10 23:14:15 -0600

    Creacion Estrategia cautelosa

---
+++ JuegoDeCartas/Blackjack/Estrategiacautelosa.cs
@@ -0,0 +1,32 @@
+using JuegoDeCartas.Interfaces; 
+
+public class EstrategiaCautelosa : IEstrategiaJuego
+{
+    private int puntoCorte;
+
+    public EstrategiaCautelosa(int puntoCorte)
+    {
+        this.puntoCorte = puntoCorte;
+    }
+
+   
+    public IEstrategiaJuego.AccionJuego DecidirAccion(Jugador jugador, Juego juego)
+    {
+        int puntosActuales = juego.CalcularPuntos(jugador);
+
+        if (puntosActuales < puntoCorte)
+        {
+            return IEstrategiaJuego.AccionJuego.PedirCarta;
+        }
+        else
+        {
+            return IEstrategiaJuego.AccionJuego.Quedarse;
+        }
+    }
+
+   
+    public Carta SeleccionarCarta(Jugador jugador, Juego juego)
+    {
+        return null;
+    }
+}
